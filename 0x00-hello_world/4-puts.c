8e12994264e4b40cb8763e93ab714fe09b5d12d9
Prestige <nonsosunday002@gmail.com>
2022-09-08 11:34:02 -0700

    updated puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * the function which carries the main
+ * then print the description using puts
+ * return "0"
+ */
+int main(void)
+{
+
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
