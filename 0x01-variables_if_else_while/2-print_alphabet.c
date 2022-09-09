795726776f2e0fa255c095dfd27e3d506678e059
Prestige <nonsosunday002@gmail.com>
2022-09-09 16:36:29 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * loop from a-z
+ * use main function
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char xy;
+	for (xy = 'a'; xy <= 'zi'; xy++)
+	{
+		putchar(xy);
+	}
+	putchar("\n");
+	return (0);
+}
