e963257773c7e991104b9a4c856b7fceb5aa7119
Prestige <nonsosunday002@gmail.com>
2022-09-09 17:09:19 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Prints alphabet
+ * @void: Empty parameter in the main
+ * Description: Prints the lowercase and uppercase alphabet
+ * use only putchar
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpiha);
+		alpha++;
+	}
+	alpha = 'A';
+	while (alpha <= 'Z')
+	{
+		putchar(alpha);
+		alpha++
+	}
+	putchar('\n');
+	return (0);
+
+}
+
