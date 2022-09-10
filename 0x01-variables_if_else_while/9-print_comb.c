73a6834a9b2dd3eec10ac2fff28679f946574f6d
Prestige <nonsosunday002@gmail.com>
2022-09-09 19:21:14 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all posible combinations of single-digit
+ * separated by ,
+ * Description: should be printed in ascending order
+ * print through putchar
+ *
+ * Return:zero
+ */
+int main(void)
+{
+int b = 0;
+	while (c < 10)
+	{
+		putchar(48 + c);
+	if (c != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
