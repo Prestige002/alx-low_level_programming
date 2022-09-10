79cdd7597d962e626ba72464c1e2545c61dfd229
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-10 04:45:23 +0100

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,27 @@
 int main(void)
 {
 int a, b;
-	for (a = 0; a <= 98; a++)
+	for (a = 0; a <= 99; a++)
 	{
-	putchar((a / 10) + '0');
-	putchar((a % 10) + '0');
-	putchar(' ');
-	putchar((b / 10) + '0');
-       	putchar((b % 10) + '0');
+		for (b = 0; b <= 99; b++)
+		{
+			for (i < j)
+				
+			{
+				putchar((a / 10) + '0');
+				putchar((a % 10) + '0');
+				putchar(' ');
+				putchar((b / 10) + '0');
+       				putchar((b % 10) + '0');
 
-	if (a == 98 && b == 99)
-	continue;
-	{
-		putchar(',');
-		putchar(' ');
-	}
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+			
 	}
 	putchar('\n');
 	return (0);
