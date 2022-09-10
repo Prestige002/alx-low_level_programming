e52df91f3662ff2fb356fcff335668e67b4c59d5
Prestige <nonsosunday002@gmail.com>
2022-09-09 18:35:13 -0700

    print tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the lowercase alphabet in reverse
+ * followed by new line
+ * putchar to prin
+ *
+ * Return: zero
+ */
+int main(void)
+{
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
