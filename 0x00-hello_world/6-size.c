a4412c15738122744e23a9569754506a1eacaa56
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-10 11:57:26 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,15 @@
  * main - main entry
  *
  * Return: return to zero
- *
  */
 int main(void)
 {	
-	int charSizes = sizeof(char);
- 	int intSizes = sizeof(int);
- 	int longSizes = sizeof(long int);
- 	int longlongSizes = sizeof(long long int);
- 	int floatSizes = sizeof(float);
- 
-	printf("Size of a char: %d byte(s)\n", charSizes);
+int charSizes = sizeof(char);
+int intSizes = sizeof(int);
+int longSizes = sizeof(long int);
+int longlongSizes = sizeof(long long int);
+int floatSizes = sizeof(float);
+        printf("Size of a char: %d byte(s)\n", charSizes);
 	printf("Size of an int: %d byte(s)\n", intSizes);
 	printf("Size of a long int: %d byte(s)\n", longSizes);
 	printf("Size of a long long int: %d byte(s)\n", longlongSizes);
