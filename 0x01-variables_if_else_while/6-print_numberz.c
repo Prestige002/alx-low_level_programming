da250337282e4a5bbb24404dd182deac5a7be0d9
Prestige <nonsosunday002@gmail.com>
2022-09-09 18:12:10 -0700

    print numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print all single digit number of base 10
+ * starting from 0
+ * followed by new line
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar("%i", i);
+	}
+	putchar("\n");
+	return (0);
+}
