c8bf248b0ca8775e92a1d76ecc151c3b79fe18e5
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-13 23:47:49 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -8,16 +8,13 @@
  */
 int _abs(int n)
 {
-	if (n >= 0)
+	if (n < 0)
 	{
-		return (n * (-1));
-	}
-	else if (n == 0)
-	{
-		return (0);
+		return (-n);
 	}
 	else
 	{
-	return (n);
+		return (n);
 	}
+return (0);
 }
