a325429928ff4148ea1e16dcd9360d84e698692e
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-13 20:44:42 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,12 +11,9 @@ void print_alphabet_x10(void)
 {
 int k;
 char i
-	for (k = 0; k <= 9; k++)
-	{
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		_putchar(i);
+		_putchar(i *= 10);
 	}
 		_putchar('\n');
-	}
 }
