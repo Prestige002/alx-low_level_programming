f1632a1cd547014d8ed787189bf35e507b89c5ad
Prestige <nonsosunday002@gmail.com>
2022-09-13 13:53:58 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - print the alphabet both uppercase and lowercase
+ * @c: loop through the lower and uppercase
+ * Description: print lower or upper case
+ *
+ * Result: 1 or 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+ 	{
+ 		return (1);
+ 	}
+	else
+		return (0);
+}
