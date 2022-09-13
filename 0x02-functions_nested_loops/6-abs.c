716cb98ffb380baaa2b885bff7f33d5c10810c39
Prestige <nonsosunday002@gmail.com>
2022-09-13 15:37:49 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - print the abs value of an integer
+ * @n: loop through if statement
+ *
+ * Return: 0
+ */
+int _abs(int)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+return (0);
+}
