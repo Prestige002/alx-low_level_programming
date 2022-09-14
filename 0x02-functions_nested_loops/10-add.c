716bd2b338d9b0c00e69e67080d3ad88c845ec52
Prestige <nonsosunday002@gmail.com>
2022-09-13 20:41:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - print the sum of two integer
+ * @n: sum the possible two number
+ * Description: add n + m
+ *
+ * Return: the result
+ */
+int add(int n, int m)
+{
+	return (n + m);
+}
