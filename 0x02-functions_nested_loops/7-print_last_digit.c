89639594d1bc28cca7d7be73d0db48628c6f440e
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-14 06:03:39 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,12 +7,22 @@
 * Return: declaration
 */
 int print_last_digit(int n)
+
 {
-int last_digit = n % 10;
-	if (last_digit < 0)
+	int last_digit;
+
+	if (n < 0)
 	{
-	 last_digit *= -1;
+		last_digit = (-1 * (n % 10));
+		_putchar (last_digit + '0');
+		return (last_digit);
 	}
-	_putchar(last_digit + '0');
-	return (last_digit);
+
+	else
+	{
+		last_digit = (n % 10);
+		_putchar (last_digit + '0');
+		return (last_digit);
+	}
+
 }
