61595905e07b23a48e6755ebdd39f1690410770f
Prestige <nonsosunday002@gmail.com>
2022-09-15 19:43:56 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 expect 2 and 4
+ *
+ */
+void print_most_numbers(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+	if (j != 50 && j != 52)
+	{
+		continue;
+	}
+		putchar(j);
+	}
+	putchar('\n');
+}
