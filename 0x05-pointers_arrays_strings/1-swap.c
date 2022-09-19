933ee4cf85b20d4fdde31d81ae13532c86ed1466
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-19 23:42:54 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 /**
  * swap_int - swap the value of two integers
  * @swap: declare swap
- * @*a: first declare
- * @*b: second declare
+ * @a: first declare
+ * @b: second declare
  * Return: always 0
  */
 void swap_int(int *a, int *b)
