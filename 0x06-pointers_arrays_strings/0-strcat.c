1d383e1f1ce147ccc7522d130640c3a46db3db62
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-25 19:51:54 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,14 +14,13 @@ char *_strcat(char *dest, char *src)
 	i = 0;
 	
 	while (dest[i] != '\0')
+	{
 		i++;
-
+	}
 	for (j = 0; src[j] != '\0'; j++)
 	{
 		dest[i + j] = src[j];
-		i++;
 	}
-
 	dest[i + j] = '\0';
 
 	return (dest);
