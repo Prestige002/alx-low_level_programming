12e68ef844b448c688730cce394e13f97cc48fa3
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-25 19:33:34 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,25 +7,19 @@ include <stdio.h>
  *@src: A pointer to a char
  *Return: dest
  */
-
 char *_strcat(char *dest, char *src)
 {
-int i, j;
+	int i, j;
 
-i = 0;
-while (dest[i] != '\0')
-{
-i++;
-}
+	for (i = 0; *dest != 0; i++)
+		dest++;
 
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-
-return (dest);
+	for (j = 0; src[j] != 0; j++)
+	{
+		*dest = src[j];
+		dest++;
+	}
+	*dest = '\0';
+	dest -= (i + j);
+	return (dest);
 }
