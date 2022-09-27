439bbf9182fd4eee59404261a535604f73a6716e
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-09-27 01:02:59 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_memset - fills memory with a constant byte
  *@s: pointer block source
@@ -6,16 +7,16 @@
  *@n: bytes of the memory
  *Return: dest
  */
-
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
+	/*Delacring FOR*/
 	for (i = 0; i < n; i++)
 	{
-		*(s + i) = b;
+		*(s + i) = b; /*add 1 position s*/
 
-	} 
+	} /*END FOR*/
+
 	return (s);
 }
