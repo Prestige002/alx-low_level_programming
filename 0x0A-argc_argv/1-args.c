5cf592135349b3e7bea3379f77cfbed0fc3932b4
Prestige <nonsosunday002@gmail.com>
2022-10-02 07:30:33 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: params int
+ * @argv: params
+ *
+ * Return 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+}
