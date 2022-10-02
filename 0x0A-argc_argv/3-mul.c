0c9db574d00c546f178660c12ce4104cfbf84e1d
Prestige <nonsosunday002@gmail.com>
2022-10-02 09:11:19 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int atoi(const char *nptr);
+/**
+ * main - multiplies two number
+ * @argc: params
+ * @argv: params
+ *
+ * Return: 1
+ */
+int main(int argc, char *argv[])
+{
+	int i, num1 = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+		num1 = num1 * atoi(argv[i]);
+		}
+		printf("%d\n", num1);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+
+}
