a4a13b20742be7dbb2ebf2b9ed0e36201dc46d66
Prestige <nonsosunday002@gmail.com>
2022-10-04 10:07:40 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *array;
-       
+
 	array = malloc(sizeof(char) * size);
 	if (size == 0 || array == NULL)
 		return (NULL);
