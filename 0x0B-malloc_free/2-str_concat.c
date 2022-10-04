c8eef82b3a5fa0ff3b452b3cf5f693ecdadd5035
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-10-04 22:26:11 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,9 +23,8 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	for (i = 0; i < l1; i++)
-	{
 		str[i] = s1[i];
+	for (i = 0; i < l2; i++)
 		str[l1 + i] = s2[i];
-	}
 	return (str);
 }
