0b9c8e15d787281575292bc71457535e2acd056e
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-10-06 03:33:52 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,41 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-/**
- * argstostr -concatenates all the arguments of your program
- * @ac: first argument passed
- * @av: double pointer array
- *
- * Return: NULL or 0
- */
-char *argstostr(int ac, char **av)
-{
-	char *str;
-	int i, j, index = 0, size = ac;
-
-	if (ac == 0 || av == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			size++;
-	}
-	str = malloc(sizeof(str*) * size + 1);
-	if (str == NULL)
-		return (NULL);
-
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; av[i][j]; j++)
-			str[index++] = av[i][j];
-		str[index++] = '\n';
-	}
-	str[size] = '\0';
-	return (str);
-}
-#include "main.h"
 /**
  * argstostr - prints args
  * @ac: takes in width of grid
