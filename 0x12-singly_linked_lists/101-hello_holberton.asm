9fe4075db6ccdddcd0de533fe2c845606dbea910
Prestige002 <106824450+Prestige002@users.noreply.github.com>
2022-10-24 02:50:22 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,28 +1,15 @@
------------------------------------------------------------------------------------
-;
-; A 64-bit program in assembly that prints Hello, Holberton, followed by a new line.
-; Compiled using nasm and gcc.
-; nasm -f elf64 101-hello_holberton.asm && gcc 101-hello_holberton.o -o hello
-;
-;--------------------------------------------------------------------------------------
+SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
 
-section .data
-format db "%s", 10, 0
-message db "Hello, Holberton", 0
-
-section .text
-extern printf ;C function to be called
-global main ;main function
-
+	SECTION .text
+	extern printf
+	global main
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, message
-	mov rax, 0 ;can be xor rax, rax
-	call printf ;call C function.
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-	pop rbp ;restor stack
-
-	mov rax, 0 ;normal, no error, return value
-	ret ;return
-
+	mov eax, 0
+	ret
